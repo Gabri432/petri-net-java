da0f9bca89a8db2c146760b93c60182c9de89dcf
Gabriele Gatti <gabrielegatti432@gmail.com>
2025-02-18 19:05:48 +0100

    Allowing to specify a name for the Petri net.

--- src/main/java/org/gabriMiao/personal/interfaces/PetriNetBuilder.java
+++ src/main/java/org/gabriMiao/personal/interfaces/PetriNetBuilder.java
@@ -3,6 +3,7 @@ package org.gabriMiao.personal.interfaces;
 import org.jetbrains.annotations.NotNull;
 
 public interface PetriNetBuilder {
+    void setPetriNetName(@NotNull String name);
     void createPlace(@NotNull String name, int tokens);
     void createTransition(@NotNull String name);
     void createArch(int weight, Place place, Transition transition, boolean inOrOut);
