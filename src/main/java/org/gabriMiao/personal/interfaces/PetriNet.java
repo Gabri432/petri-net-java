b4a9d9800856dfaf3edacb1c1232cb2f33ebcdf3
Gabriele Gatti <gabrielegatti432@gmail.com>
2025-02-24 15:41:42 +0100

    Removing unused import.

--- src/main/java/org/gabriMiao/personal/interfaces/PetriNet.java
+++ src/main/java/org/gabriMiao/personal/interfaces/PetriNet.java
@@ -2,8 +2,6 @@ package org.gabriMiao.personal.interfaces;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 public interface PetriNet {
 
     void fireTransition(@NotNull String transitionName);
